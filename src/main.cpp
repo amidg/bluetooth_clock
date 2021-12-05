a0cce0dec776d5a062b539694773085edac8a580
Dmitrii Gusev <amidg4x4dima@gmail.com>
2021-12-04 19:31:55 -0500

    firmware started

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+/*
+
+  BLUETOOTH CLOCK WITH WI-FI / BLUETOOTH SPEAKER AND NEOPIXEL RGB MATRIX
+
+  Developed by Dmitrii Gusev.
+  github.com/amidg
+
+  This firmware contains products developed by Adafruit, please, buy their work! 
+
+  This firmware is designed to run on custom PCB by Dmitrii Gusev. Performance on other platforms is not guaranteed.
+
+*/
+
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
